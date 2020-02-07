d140b2f8f1c36fb3f9e7086943ee8a967ef1d805
durga prasad thota <durga.thota@purviewservices.com>
2020-02-07 08:54:18 +0530

    make it better

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,7 +2,6 @@ import React,{useState,useEffect} from 'react';
 import { Typography } from 'antd';
 import { Row, Col ,Card,Menu,Button,Icon,Form} from 'antd';
 import axios from 'axios';
-
 const { Paragraph } = Typography;
 const { Text } = Typography;
 const { Title } = Typography;
@@ -12,25 +11,26 @@ export default function Profile (){
     let [edit,setEdit]=useState(true);
     let [hide,setHide]=useState(true)
     let[profile,setProfile]=useState([]);
-    let name="sreedhar"
 
     let key=localStorage.getItem("token");
 useEffect(()=>{
 axios.get("https://evd-project.herokuapp.com/api/current_user_profile/",
 {headers: {'content-type':'application/json','Authorization':`Token ${key}` }})
-.then (resp=>setProfile(resp.data))
+.then (resp=>{setProfile(resp.data)
+setUsername(resp.data.username)
+setEmail(resp.data.email)
+}
+)
 },[])
-
-
 let editClickHandler=(username,email)=>{
   setEdit(false);
   setHide(false);
-setUsername(username)
-setEmail(email)
 }
 let handleSubmit=()=>{
   // alert(username)
+
 }
+
    return(
         <div >
          <Row>
@@ -49,37 +49,32 @@ let handleSubmit=()=>{
            </div>
          ):(
            <div>
-           <from onSubmit={handleSubmit}>
+           <form onSubmit={handleSubmit}>
            <input 
-           value={profile.email}
+           value={email}
            type="text"
            onChange={e=>setEmail(e.target.value)} >
            
            </input> <br/><br/>
            <input
            type="text"
+           value={username}
            onChange={e=>setUsername(e.target.value)}>
            </input>  <br/><br/>
            
-           </from>
+           </form>
            </div>
          )}
           <div>
-       
        </div>
-       
-     
-   
       { edit? (
         //  <Button onClick={editClickHandler}>edit</Button>
         <Icon style={{fontSize:"x-large"}} onClick={e=>editClickHandler(profile.username,profile.email)} type="edit" />
          ):(
         //  <Button>update</Button>
-        <Icon style={{fontSize:"x-large",float:"right"}} type="save" />)
+        <Icon style={{fontSize:"x-large",float:"right"}}  type="save" />)
       }
     </Card>
-    
-    
       </Col>
       <Col xs={1} sm={1} md={6} lg={8} xl={8}></Col>
     </Row>
